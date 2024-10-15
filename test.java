ad5ea383af3d4979ec65044ef2b4d6a80e927689
HYxiaoyuan <1312973718@qq.com>
2024-10-15 20:27:04 +0800

    第一版

---
+++ test.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class test {
+    public void aaa(){
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+
+    }
+}
